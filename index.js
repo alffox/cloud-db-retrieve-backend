6f107521bde722b3b926a141e5b41178af59129e
alffox <alfonso.crisci.it@gmail.com>
2021-03-05 09:08:38 +0100

    feat: Get json data

--- index.js
+++ index.js
@@ -32,16 +32,44 @@ app.get("/dblist", function (req, res) {
 
         res.on("end", function () {
             var authToken = Buffer.concat(chunks);
-            console.log(authToken.toString());
             var authTokenJSON = JSON.parse(authToken);
             var token = authTokenJSON.token;
-            console.log(token);
+
+            var options = {
+                "method": "GET",
+                "hostname": "swarm.liferay.int",
+                "port": "55430",
+                "path": "/v3/registry/active",
+                "headers": {
+                    "content-type": "application/json",
+                    "authorization": `Bearer ${token}`
+                }
+            };
+
+            var req = http.request(options, function (res) {
+                var chunks = [];
+
+                res.on("data", function (chunk) {
+                    chunks.push(chunk);
+                });
+
+                res.on("end", function () {
+                    var agents = Buffer.concat(chunks);
+                    //console.log(agents.toString());
+                    var agentsJSON = JSON.parse(agents);
+                    console.log(agentsJSON);
+
+                });
+            });
+
+            req.end();
         });
     });
 
     req.write(JSON.stringify({ email: email }));
     req.end();
     res.end();
+
 });
 
 app.listen(port, function () {
